e4d20fcd20bf569f8f5f3e87b626ee0f3bc751d7
Marcus Vinícius <marcus-vnt@live.com>
2024-02-29 16:52:35 -0300

    style: CSS reset.

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { darkModeTheme } from "./theme.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkModeTheme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
