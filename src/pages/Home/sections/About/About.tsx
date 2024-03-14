46b266716d2f5ce3fc180eb4081a83d20b45749e
Marcus Vinícius <marcus-vnt@live.com>
2024-03-13 21:14:50 -0300

    feat: About Me: Experience and Education added.

--- src/pages/Home/sections/About/About.tsx
+++ src/pages/Home/sections/About/About.tsx
@@ -1,4 +1,6 @@
-import { Container, Typography, styled } from "@mui/material";
+import { Grid, Container, Typography, styled, Box } from "@mui/material";
+import WorkspacePremiumIcon from "@mui/icons-material/WorkspacePremium";
+import SchoolIcon from "@mui/icons-material/School";
 
 const About = () => {
   const StyledAbout = styled("section")(({}) => ({
@@ -12,9 +14,74 @@ const About = () => {
   return (
     <StyledAbout>
       <Container maxWidth="lg">
-        <Typography variant="h1" fontFamily="Inter" textAlign="center">
+        <Typography
+          variant="h1"
+          fontFamily="Inter"
+          textAlign="center"
+          marginBottom="4rem"
+        >
           About Me
         </Typography>
+        <Grid
+          container
+          sx={{
+            justifyContent: "center",
+            gap: "3rem",
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              border: "1px solid #000",
+              borderRadius: "4px",
+              padding: "1rem",
+              gap: ".25rem",
+              width: "203px",
+            }}
+          >
+            <WorkspacePremiumIcon />
+            <Typography variant="h6" fontWeight="bold">
+              Experience
+            </Typography>
+            <Typography variant="body1" paragraph={true} marginBottom="0">
+              Software Developer
+            </Typography>
+            <Typography variant="body1" paragraph={true} marginBottom="0">
+              Front-End
+            </Typography>
+            <Typography variant="body1" paragraph={true} marginBottom="0">
+              2 Years of Improvement
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              border: "1px solid #000",
+              borderRadius: "4px",
+              padding: "1rem",
+              gap: ".25rem",
+              width: "203px",
+            }}
+          >
+            <SchoolIcon />
+            <Typography variant="h6" fontWeight="bold">
+              Education
+            </Typography>
+            <Typography variant="body1" paragraph={true} marginBottom="0">
+              Bachelors Degree
+            </Typography>
+            <Typography variant="body1" paragraph={true} marginBottom="0">
+              Software Engineering
+            </Typography>
+            <Typography variant="body1" paragraph={true} marginBottom="0">
+              (2022-2026)
+            </Typography>
+          </Box>
+        </Grid>
       </Container>
     </StyledAbout>
   );
