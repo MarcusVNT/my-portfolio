4e7323b7c3c4bc71e4852ca7db99090ada0300b0
Marcus Vinícius <marcus-vnt@live.com>
2024-04-08 17:44:00 -0300

    prototype of the layout.

--- src/pages/Home/sections/Projects/Projects.tsx
+++ src/pages/Home/sections/Projects/Projects.tsx
@@ -7,6 +7,7 @@ const Projects = () => {
     display: "flex",
     alignItems: "center",
     justifyContent: "center",
+    paddingBottom: "64px", //retirar depois de desenvolver a section.
 
     // [theme.breakpoints.down("xs")]: {
     //   margin: "2rem 1rem",
@@ -26,10 +27,49 @@ const Projects = () => {
           Projects
         </Typography>
 
-        <Grid>
-          <Stack border="1px solid #fff">
-            <img src={Skyfall} width="300px" />
-          </Stack>
+        <Grid container gap="48px" justifyContent="center">
+          <Grid border="1px solid #fff" flexDirection="column" item xs={5}>
+            <Grid>
+              <img src={Skyfall} width="100%" />
+            </Grid>
+            <Stack p={3} alignItems="center" justifyContent="center">
+              <Typography
+                variant="h2"
+                color="primary.contrastText"
+                fontFamily="Inter"
+                textAlign="center"
+                marginBottom="16px"
+              >
+                Skyfall
+              </Typography>
+              <Typography color="primary.contrastText">
+                Skyfall is a project that I developed to help people to find
+                their dream home. It's a web application that uses the ZAP API
+                to get the properties and show them in a beautiful way.
+              </Typography>
+            </Stack>
+          </Grid>
+          <Grid border="1px solid #fff" flexDirection="column" item xs={5}>
+            <Grid>
+              <img src={Skyfall} width="100%" />
+            </Grid>
+            <Stack p={3} alignItems="center" justifyContent="center">
+              <Typography
+                variant="h2"
+                color="primary.contrastText"
+                fontFamily="Inter"
+                textAlign="center"
+                marginBottom="16px"
+              >
+                Skyfall
+              </Typography>
+              <Typography color="primary.contrastText">
+                Skyfall is a project that I developed to help people to find
+                their dream home. It's a web application that uses the ZAP API
+                to get the properties and show them in a beautiful way.
+              </Typography>
+            </Stack>
+          </Grid>
         </Grid>
       </Container>
     </StyledProjects>
