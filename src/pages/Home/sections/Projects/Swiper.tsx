f219e4de4a05d26dc53988c3399264957db94af1
Marcus Vinícius <marcus-vnt@live.com>
2024-04-24 15:16:10 -0300

    feat: change the carousel.

--- src/pages/Home/sections/Projects/Swiper.tsx
+++ src/pages/Home/sections/Projects/Swiper.tsx
@@ -1,162 +1,37 @@
-import { Swiper, SwiperSlide } from "swiper/react";
 import Skyfall from "../../../../assets/images/skyfall.jpg";
+import Arvore from "../../../../assets/images/arvore.jpg";
+import Pitu from "../../../../assets/images/pitu (1).jpg";
 import { Grid, Stack, Typography } from "@mui/material";
-import { A11y, Navigation, Pagination, Scrollbar } from "swiper/modules";
+import Carousel from "react-material-ui-carousel";
 
 export default function SwiperComponent() {
+  const images = [Skyfall, Arvore, Pitu];
+
   return (
-    <>
-      <Swiper
-        modules={[Navigation, Pagination, Scrollbar, A11y]}
-        spaceBetween={48}
-        slidesPerView={3}
-        navigation
-        pagination={{ clickable: true }}
-        scrollbar={{ draggable: true }}
-        onSwiper={(swiper) => console.log(swiper)}
-        onSlideChange={() => console.log("slide change")}
-      >
-        <Grid container gap="48px" justifyContent="center">
-          <SwiperSlide>
-            <Grid border="1px solid #fff" flexDirection="column" item sm={5}>
-              <Grid>
-                <img src={Skyfall} width="100%" />
-              </Grid>
-              <Stack p={3} alignItems="center" justifyContent="center">
-                <Typography
-                  variant="h2"
-                  color="primary.contrastText"
-                  fontFamily="Inter"
-                  textAlign="center"
-                  marginBottom="16px"
-                >
-                  Skyfall
-                </Typography>
-                <Typography color="primary.contrastText">
-                  Skyfall is a project that I developed to help people to find
-                  their dream home. It's a web application that uses the ZAP API
-                  to get the properties and show them in a beautiful way.
-                </Typography>
-              </Stack>
-            </Grid>
-          </SwiperSlide>
-          <SwiperSlide>
-            <Grid border="1px solid #fff" flexDirection="column" item sm={5}>
-              <Grid>
-                <img src={Skyfall} width="100%" />
-              </Grid>
-              <Stack p={3} alignItems="center" justifyContent="center">
-                <Typography
-                  variant="h2"
-                  color="primary.contrastText"
-                  fontFamily="Inter"
-                  textAlign="center"
-                  marginBottom="16px"
-                >
-                  Skyfall
-                </Typography>
-                <Typography color="primary.contrastText">
-                  Skyfall is a project that I developed to help people to find
-                  their dream home. It's a web application that uses the ZAP API
-                  to get the properties and show them in a beautiful way.
-                </Typography>
-              </Stack>
-            </Grid>
-          </SwiperSlide>
-          <SwiperSlide>
-            <Grid border="1px solid #fff" flexDirection="column" item sm={5}>
-              <Grid>
-                <img src={Skyfall} width="100%" />
-              </Grid>
-              <Stack p={3} alignItems="center" justifyContent="center">
-                <Typography
-                  variant="h2"
-                  color="primary.contrastText"
-                  fontFamily="Inter"
-                  textAlign="center"
-                  marginBottom="16px"
-                >
-                  Skyfall
-                </Typography>
-                <Typography color="primary.contrastText">
-                  Skyfall is a project that I developed to help people to find
-                  their dream home. It's a web application that uses the ZAP API
-                  to get the properties and show them in a beautiful way.
-                </Typography>
-              </Stack>
-            </Grid>
-          </SwiperSlide>
-          <SwiperSlide>
-            <Grid border="1px solid #fff" flexDirection="column" item sm={5}>
-              <Grid>
-                <img src={Skyfall} width="100%" />
-              </Grid>
-              <Stack p={3} alignItems="center" justifyContent="center">
-                <Typography
-                  variant="h2"
-                  color="primary.contrastText"
-                  fontFamily="Inter"
-                  textAlign="center"
-                  marginBottom="16px"
-                >
-                  Skyfall
-                </Typography>
-                <Typography color="primary.contrastText">
-                  Skyfall is a project that I developed to help people to find
-                  their dream home. It's a web application that uses the ZAP API
-                  to get the properties and show them in a beautiful way.
-                </Typography>
-              </Stack>
-            </Grid>
-          </SwiperSlide>
-          <SwiperSlide>
-            <Grid border="1px solid #fff" flexDirection="column" item sm={5}>
-              <Grid>
-                <img src={Skyfall} width="100%" />
-              </Grid>
-              <Stack p={3} alignItems="center" justifyContent="center">
-                <Typography
-                  variant="h2"
-                  color="primary.contrastText"
-                  fontFamily="Inter"
-                  textAlign="center"
-                  marginBottom="16px"
-                >
-                  Skyfall
-                </Typography>
-                <Typography color="primary.contrastText">
-                  Skyfall is a project that I developed to help people to find
-                  their dream home. It's a web application that uses the ZAP API
-                  to get the properties and show them in a beautiful way.
-                </Typography>
-              </Stack>
-            </Grid>
-          </SwiperSlide>
-          <SwiperSlide>
-            <Grid border="1px solid #fff" flexDirection="column" item sm={5}>
-              <Grid>
-                <img src={Skyfall} width="100%" />
-              </Grid>
-              <Stack p={3} alignItems="center" justifyContent="center">
-                <Typography
-                  variant="h2"
-                  color="primary.contrastText"
-                  fontFamily="Inter"
-                  textAlign="center"
-                  marginBottom="16px"
-                >
-                  Skyfall
-                </Typography>
-                <Typography color="primary.contrastText">
-                  Skyfall is a project that I developed to help people to find
-                  their dream home. It's a web application that uses the ZAP API
-                  to get the properties and show them in a beautiful way.
-                </Typography>
-              </Stack>
-            </Grid>
-          </SwiperSlide>
+    <Carousel>
+      {images.map((image, index) => (
+        <Grid key={index} border="1px solid #fff" height="50%" width="50%">
+          <Grid>
+            <img src={image} width="100%" />
+          </Grid>
+          <Stack p={3} alignItems="center" justifyContent="center">
+            <Typography
+              variant="h2"
+              color="primary.contrastText"
+              fontFamily="Inter"
+              textAlign="center"
+              marginBottom="16px"
+            >
+              Skyfall
+            </Typography>
+            <Typography color="primary.contrastText">
+              Skyfall is a project that I developed to help people to find their
+              dream home. It's a web application that uses the ZAP API to get
+              the properties and show them in a beautiful way.
+            </Typography>
+          </Stack>
         </Grid>
-      </Swiper>
-    </>
+      ))}
+    </Carousel>
   );
 }
