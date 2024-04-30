afbf954beeed6e0b4d071e0c0b33aca15bc3e5b5
Marcus Vinícius <marcus-vnt@live.com>
2024-04-30 13:10:27 -0300

    adding AudioNotes project.

--- src/pages/Home/sections/Projects/Cards.tsx
+++ src/pages/Home/sections/Projects/Cards.tsx
@@ -1,5 +1,6 @@
 import { Card, CardContent, Grid, Stack, Typography } from "@mui/material";
 import Dogs from "../../../../../public/dogs.png";
+import AudioNotes from "../../../../../public/audioNotes.png";
 import Skyfall from "../../../../../public/skyfall.jpg";
 
 export default function Cards() {
@@ -26,14 +27,14 @@ export default function Cards() {
                 Dogs | The Social Media For Pets
               </Typography>
               <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                This is a project for a social network for pets, where pet
-                owners can create a profile for their pets and interact with
-                other pet owners. It was developed using ReactJS, JavaScript,
-                Node.js, CSS modules and other technologies. It's one of the
-                most complex and complete projects I've ever done, and I was
-                able to learn about using and integrating APIs, authenticating
-                users, creating comments and many other features that we deal
-                with in day-to-day development.
+                This is a project for a social media for pets, where pet owners
+                can create a profile for their pets and interact with other pet
+                owners. It was developed using ReactJS, JavaScript, Node.js, CSS
+                modules and other technologies. It's one of the most complex and
+                complete projects I've ever done, and I was able to learn about
+                using and integrating APIs, authenticating users, creating
+                comments and many other features that we deal with in day-to-day
+                development.
               </Typography>
             </Stack>
           </CardContent>
@@ -47,8 +48,8 @@ export default function Cards() {
         >
           <CardContent>
             <img
-              src={Skyfall}
-              alt="Skyfall"
+              src={AudioNotes}
+              alt="AudioNotes"
               style={{
                 objectFit: "contain",
                 width: "100%",
@@ -57,11 +58,15 @@ export default function Cards() {
             />
             <Stack sx={{ padding: "16px 16px 0 16px" }}>
               <Typography variant="h5" component="div">
-                Lizard
+                AudioNotes
               </Typography>
               <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                Lizards are a widespread group of squamate reptiles, with over
-                6,000 species, ranging across all continents except Antarctica
+                AudioNotes is a project that I developed to practice my skills
+                and learn more about React.JS, Typescript and some libery like
+                Radix and other ones. The Project was developed on the Next
+                Level Week #5 from Rocketseat. AudioNotes is a simple web
+                aplication that you can create notes using your keyboard or your
+                voice and visualize it in a list of cards.
               </Typography>
             </Stack>
           </CardContent>
