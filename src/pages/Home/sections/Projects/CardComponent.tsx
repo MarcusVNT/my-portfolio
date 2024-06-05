83415374c7bad3bbd5e8f8b92ec86c799ba86520
Marcus Vinícius <marcus-vnt@live.com>
2024-06-04 23:14:43 -0300

    Implementing the new cards in the Project section..

---
+++ src/pages/Home/sections/Projects/CardComponent.tsx
@@ -0,0 +1,67 @@
+import { FC, useState } from "react";
+import { CardInfo } from "./CardsData";
+import {
+  Card,
+  CardContent,
+  Grid,
+  Link,
+  Stack,
+  Typography,
+} from "@mui/material";
+
+const modalStyle = {
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #4f8e3e",
+  boxShadow: 24,
+  pt: 2,
+  px: 4,
+  pb: 3,
+};
+
+const CardComponent: FC<CardInfo> = ({ title, image, description, link }) => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  return (
+    <Grid item sm={5}>
+      <Card
+        sx={{
+          borderRadius: "8px",
+          height: "400px",
+          boxShadow: "inset 0px -100px 20px 0px rgba(0, 0, 0, 0.5)",
+          cursor: "pointer",
+        }}
+        onClick={handleOpen}
+      >
+        <CardContent>
+          <img
+            src={image}
+            alt={title}
+            style={{
+              objectFit: "contain",
+              width: "100%",
+              maxHeight: "200px",
+              borderRadius: "8px 8px 0 0",
+            }}
+          />
+          <Stack sx={{ padding: "16px 16px 0 16px" }}>
+            <Typography variant="h5" component="div" marginBottom="8px">
+              {title}
+            </Typography>
+            <Typography sx={{ mb: 1.5 }} color="text.secondary">
+              {description}
+            </Typography>
+          </Stack>
+          <Link href={link} underline="none" target="_blank" rel="noreferrer" />
+        </CardContent>
+      </Card>
+    </Grid>
+  );
+};
+
+export default CardComponent;
