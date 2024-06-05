d520c7c20dca5dc41ff2c43337ae1ac6d0cb468b
Marcus Vinícius <marcus-vnt@live.com>
2024-06-05 16:52:09 -0300

    Github's link added.

--- src/pages/Home/sections/Projects/Cards.tsx
+++ src/pages/Home/sections/Projects/Cards.tsx
@@ -1,13 +1,4 @@
-import {
-  Card,
-  CardContent,
-  Grid,
-  Link,
-  Modal,
-  Stack,
-  Typography,
-  styled,
-} from "@mui/material";
+import { Grid, Link, Typography, styled } from "@mui/material";
 import { cardsData, CardInfo } from "./CardsData";
 import CardComponent from "./CardComponent";
 
@@ -21,25 +12,16 @@ export default function Cards() {
     },
   });
 
-  const modalStyle = {
-    position: "absolute" as "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    width: 400,
-    bgcolor: "background.paper",
-    border: "2px solid #000",
-    boxShadow: 24,
-    pt: 2,
-    px: 4,
-    pb: 3,
-  };
-
   return (
     <Grid container gap="48px" justifyContent="center">
       {cardsData.map((card: CardInfo, index: number) => (
         <CardComponent key={index} {...card} />
       ))}
+      <Typography sx={{ color: "#fff" }}>
+        {" "}
+        Para ver mais projectos clique{" "}
+        <StyledLink href="https://github.com/MarcusVNT">aqui.</StyledLink>
+      </Typography>
     </Grid>
   );
 }
